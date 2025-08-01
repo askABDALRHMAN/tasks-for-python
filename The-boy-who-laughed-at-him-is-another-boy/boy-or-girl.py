ac713f6afdda9daab6a59684c19eeb7cf4f69968
askABDALRHMAN <aboelababid2005@gmail.com>
2025-08-01 15:15:33 +0300

    أول رفع للمشروع

---
+++ The-boy-who-laughed-at-him-is-another-boy/boy-or-girl.py
@@ -0,0 +1,9 @@
+
+
+name = input("دخل اسمك يا حبيبي: ").lower()
+chick_about_name = set(name)
+
+if len(chick_about_name) % 2 == 0:
+    print("CHAT WITH HER ===> ازيك يا حبيبي عامله ايه ممكن رقمك ههههه")
+else:
+    print("IGNORE HIM ===> وحيات امك لاقفشك يا نصاب يا منحل شخصيات البنات يا ******")
---
+++ divideEvenNumber/Watermelon.py
@@ -0,0 +1,14 @@
+
+
+
+w = int(input("hello please enter a number for chick its: "))
+
+
+if( w % 2 == 0 & w >= 4 ):
+    print("YES")
+else:
+    print("NO")
+
+
+
+    
